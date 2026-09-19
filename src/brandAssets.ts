5ac0e3a8b5cd5d0bcb5f051c96df601cbfc1e77b
JuanHeng <irene11733@gmail.com>
2026-09-19 22:54:22 +0800

    Publish reference homepage preview to project Pages

--- src/brandAssets.ts
+++
@@ -1,5 +0,0 @@
-const publicAsset = (fileName: string) => `${import.meta.env.BASE_URL}images/${fileName}`
-
-export const authSceneUrl = publicAsset('auth-scene.jpg')
-export const brandLogoUrl = publicAsset('brand-logo.png')
-export const dashboardMountainsUrl = publicAsset('dashboard-mountains.jpg')
